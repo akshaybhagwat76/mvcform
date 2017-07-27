85480cc53b918de41d02a6b4081edc1c9dbe53cf
Akshay Bhagwat <akshaybhagwat76@gmail.com>
2017-07-27 20:22:32 +0530

    formcollection

---
+++ form_Collection/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace form_Collection
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ form_Collection/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using form_Collection.Models;
+
+namespace form_Collection.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        Student_DBEntities sd = new Student_DBEntities();
+
+        public ActionResult Index()
+        {
+            ViewData["list"] = sd.students.ToList();
+            return View();
+        }
+
+        public ActionResult AddStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddStudent(FormCollection fc)
+        {
+            student s = new student();
+            s.f_name = fc["txtfn"];
+            s.age = Convert.ToInt16(fc["txtag"]);
+
+            sd.students.Add(s);
+            sd.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var data = (from n in sd.students where n.student_id == id select n).FirstOrDefault();
+            ViewData["edit"] = data;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(FormCollection fc)
+        {
+            int id = Convert.ToInt16(fc["txtid"]);
+            var data = from n in sd.students where n.student_id == id select n;
+
+            foreach (var item in data)
+            {
+                item.f_name = fc["txtfn"];
+                item.age = Convert.ToInt16(fc["txtag"]);
+            }
+
+            sd.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            student s = sd.students.Find(id);
+            sd.students.Remove(s);
+            sd.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
